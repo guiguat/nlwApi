21a5acdd3762183c8113bba611d8bb4fc63f20b2
GuilhermeGuatura <guaturadzn@gmail.com>
2020-06-03 07:01:09 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
