f8e5d7dce85215cad4270bc15866a6fa3235d596
GuilhermeGuatura <guaturadzn@gmail.com>
2020-06-03 06:32:38 -0300

    create itemsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,14 @@
 import express, { Request, Response } from 'express';
-import knex from './database/connection';
 
 import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
 
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async(req:Request, res:Response)=>{
-   const items = await knex('items').select('*');
-
-   const serializedItems = items.map(item =>{
-      return {
-         id: item.id,
-         title:item.title,
-         image_url: `http://localhost:3333/uploads/${item.image}`
-      };
-   })
-
-   return res.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 
