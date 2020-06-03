9208c68796091cbecabccafd2b625b5584067baf
GuilhermeGuatura <guaturadzn@gmail.com>
2020-06-03 06:58:44 -0300

    index points

--- src/controllers/pointsController.ts
+++ src/controllers/pointsController.ts
@@ -35,11 +35,30 @@ class PointController{
      
         await trx('point_items').insert(pointItems);
         
+        await trx.commit();
+
         return response.json({
             point_id,
             ...point
         });
      }
+
+   async index(request:Request, response:Response){
+      const {items, uf, city} = request.query;
+      const parsedItems = String(items)
+         .split(',')
+         .map(item=> Number(item.trim()));
+      const points = await knex('points')
+         .join('point_items', 'points.id', '=', 'point_items.point_id')
+         .whereIn('point_items.item_id', parsedItems)
+         .where('city', String(city))
+         .where('uf', String(uf))
+         .distinct()
+         .select('points.*');
+      
+      return response.json(points);
+   }
+
    async show(request:Request, response:Response){
       const { id } = request.params;
 
