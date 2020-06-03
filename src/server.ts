c2da1d2c071282b038b5671e0edbaf8e8d87fc55
GuilhermeGuatura <guaturadzn@gmail.com>
2020-06-03 05:23:23 -0300

    create uploads route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(routes);
 
-app.use(express.json);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
+app.use(express.json);
 app.listen(3333);
